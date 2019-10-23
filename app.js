54368c2bdf6ca30f4d089c5dc57929b24566d37f
reihned <5162283+reihned@users.noreply.github.com>
2019-10-23 16:19:31 -0400

    SSR switch from Jade to Vue, closes #6

--- app.js
+++ app.js
@@ -4,15 +4,55 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const vueRenderer = require('@doweb/vuexpress').vueRenderer;
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 var imagesRouter = require('./routes/images');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+
+let options = {
+  // folder with your views
+  views: './views',
+  // cache templates
+  cache: true,
+  // use watch = true only in dev mode! Will start webpack watcher only on the current request.
+  watch: (process.env.NODE_ENV === "development"),
+  // meta info - check out https://github.com/ktquez/vue-head for more information
+  metaInfo: {
+    title: 'demo-gallery'
+  },
+  // extract css to file, otherwise it will be inline
+  extractCSS: true,
+  // css output folder, extracted styles from your *.vue files
+  cssOutputPath: 'stylesheets/style.css',
+  // path to your web root
+  publicPath: './public',
+  // global vars, access directly like window.
+  globals: {
+    appName: 'demo-gallery'
+  },
+  plugins: [
+    // vue plugins
+    // require('your-plugin')
+  ],
+  compilerConfig: {
+    // custom webpack config
+  },
+  compilerConfigCallback: function (webpackConfig) {
+    // change the merged webpackconfig if you like
+    return webpackConfig;
+  },
+  onError: (err) => { }, // error handler
+  onReady: () => { } // ready event handler, when completed the work of initialization
+};
+const renderer = vueRenderer(options);
+app.use(renderer);
 
 app.use(logger('dev'));
 app.use(express.json());
